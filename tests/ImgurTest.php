67cc388fac835ed98d1ee00f42421c3f1682a038
Vongola <vongola12324@coder.tw>
2018-04-15 15:13:23 +0800

    Add example testcase

---
+++ tests/ImgurTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+// FIXME: Add TestCase
+class ImgurTest extends TestCase {
+
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    public function exampleTestCase()
+    {
+        $this->assertTrue(1 === 1);
+    }
+}
