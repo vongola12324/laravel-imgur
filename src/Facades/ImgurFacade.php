c678bf8500e70ea9219fb22f22a7b778c5f49fb2
vongola12324 <vongola12324@coder.tw>
2026-06-25 17:51:06 +0800

    feat: update ServiceProvider and Facade for new thin-wrapper Client

--- src/Facades/ImgurFacade.php
+++ src/Facades/ImgurFacade.php
@@ -1,14 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Vongola\Imgur\Facades;
 
 use Illuminate\Support\Facades\Facade;
-use Vongola\Imgur\Client;
 
 class ImgurFacade extends Facade
 {
     protected static function getFacadeAccessor(): string
     {
-        return Client::class;
+        return \Vongola\Imgur\Client::class;
     }
 }
